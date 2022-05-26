b331482bcea816de7b7b1fb9102388174f4d03b8
Mart <themcman@gmail.com>
2022-05-26 02:00:49 -0500

    server works

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,21 @@
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
+
 fn main() {
-    test();
+    let server = TcpListener::bind("127.0.0.1:9999").expect("should be able to open server");
+    for stream in server.incoming() {
+        spawn(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+                println!("{:#?} {}, {}", msg, msg.is_binary(), msg.is_text());
+
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+    }
 }
