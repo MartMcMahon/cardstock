595eca26eefde244cc36d18c10d7f2880a8c7ea5
Mart <themcman@gmail.com>
2022-06-06 14:20:52 -0500

    node ws test

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,32 +6,74 @@ const http = require("http");
 
 const fs = require("fs");
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
 // create a route for the app
-app.get("/", (req: any, res: any) => {
-  res.send("hi mart!!");
-});
+// app.get("/", (req: any, res: any) => {
+//   res.send("hi mart!!");
+// });
 
-app.get("/ok", (req: any, res: any) => {
-  res.send("ok....");
-});
+// app.get("/ok", (req: any, res: any) => {
+//   res.send("ok....");
+// });
+
+import env from "./.env.dev";
+const { httpPort, httpsPort, certFile, keyFile } = env;
 
 // Listen both http & https ports
-const httpServer = http.createServer();
+const httpServer = http.createServer();//app);
 const httpsServer = https.createServer(
   {
     key: fs.readFileSync("./key-rsa.pem"),
     cert: fs.readFileSync("./cert.pem"),
   },
-  app
+  // app
 );
 
-httpServer.listen(8000, () => {
-  console.log("HTTP Server running on port 80");
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ server: httpsServer });
+
+const clients = new Map();
+wss.on("connection", (ws) => {
+  ws.send("something");
+  clients.set(ws, "test");
+});
+wss.on("message", (data) => {
+  console.log("received: %s", data);
 });
 
-httpsServer.listen(8080, () => {
-  console.log("HTTPS Server running on port 443");
+setInterval(() => {
+  console.log(clients);
+}, 1000);
+
+httpsServer.listen(httpsPort, () => {
+  console.log(`HTTPS Server running on port ${httpsPort}`);
 });
+// insec = new WebSocketServer({ server: httpsServer });
+// index.on("connection");
+httpServer.listen(httpPort, () => {
+  console.log(`HTTP Server running on port ${httpPort}`);
+});
+
+// httpsServer.listen(httpsPort);
+
+// const WebSocket = require("ws").Server;
+// let socket = new WebSocket({
+//   server: httpsServer,
+// });
+
+// // server.listen(config.port);
+
+// // import ws from 'ws';
+
+// // const wss = new ws.Server({ port: 333 });
+// const clients = new Map();
+// socket.on("connection", (ws: any) => {
+//   console.log("connectred:", ws);
+//   const color = Math.floor(Math.random() * 360);
+
+//   clients.set(ws, color);
+// });
+
+// socket.listen(8080);
